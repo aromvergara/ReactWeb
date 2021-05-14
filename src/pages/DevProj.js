2c2bfd1dd70ac620683242a4046a98e9423148bd
aromvergara <aromvergara@gmail.com>
2021-05-13 19:10:30 -0500

    semifinal version

--- src/pages/DevProj.js
+++ src/pages/DevProj.js
@@ -12,15 +12,15 @@ const DevProj = () => {
               <article className="projects">
                 <div className="projects-item projects-item-01">
                   {/*img src="images/UXUIdesign.jpg" alt="UX/UI Design"*/}
-                  <a onClick={()=> window.open("https://aromvergara.github.io/Netflix-Clone", "_blank")} href="/DevProj" data-title="Website Clone"><img src="images/webclone.png" alt="Website Clone" /></a>
+                  <a onClick={()=> window.open("https://aromvergara.github.io/Netflix-Clone", "_blank")} href="/DevProj" data-title="Website Clone"><img src="images/webclone.jpeg" alt="Website Clone" /></a>
                 </div>
                 <div className="projects-item projects-item-02">
                   {/*img src="images/webdevelopment.png" alt="Web Development"*/}
-                  <a onClick={()=> window.open("https://crud-notess.herokuapp.com", "_blank")} href="/DevProj" data-title="CRUD App"><img src="images/crudapp.png" alt="CRUD App" /></a>
+                  <a onClick={()=> window.open("https://crud-notess.herokuapp.com", "_blank")} href="/DevProj" data-title="CRUD App"><img src="images/crudapp.jpeg" alt="CRUD App" /></a>
                 </div>
                 <div className="projects-item projects-item-03">
                   {/*img src="images/projectmanagement.jpg" alt="Project Management"*/}
-                  <a onClick={()=> window.open("https://weather-app-ciccc.herokuapp.com", "_blank")} href="/DevProj" data-title="Weather App"><img src="images/weatherapp.png" alt="Weather App" /></a>
+                  <a onClick={()=> window.open("https://cors-anywhere.herokuapp.com/", "_blank")} onClick={()=> window.open("https://weather-app-ciccc.herokuapp.com", "_blank")} href="/DevProj" data-title="Weather App"><img src="images/weatherapp.png" alt="Weather App" /></a>
                 </div>
               </article>
             </section>
@@ -28,7 +28,7 @@ const DevProj = () => {
               <article className="projects">
                 <div className="projects-item projects-item-03">
                   {/*img src="images/projectmanagement.jpg" alt="Project Management"*/}
-                  <a onClick={()=> window.open("https://weather-app-ciccc.herokuapp.com", "_blank")} href="/DevProj" data-title="Weather App"><img src="images/projectmanagement.jpg" alt="Weather App" /></a>
+                  <a onClick={()=> window.open("https://dan-vergara.webflow.io/", "_blank")} href="/DevProj" data-title="High performance website"><img src="images/highperformance.png" alt="High performance website" /></a>
                 </div>
               </article>
             </section>
